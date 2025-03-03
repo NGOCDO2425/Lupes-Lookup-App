6356efb4a673895291efece5ec978c9ae8780b15
NGOCDO2425 <ngdo8647@stu.sjcc.edu>
2025-03-03 01:26:23 -0800

    Fixed the path issues and tested the script

--- birthday_example.py
+++ birthday_example.py
@@ -1,22 +1,7 @@
 import json
 
-# this is a relative path to the .json data file
-# you can also use a 'full' or 'absolute path' to the file
-# windows and mac paths are different.  You should google and youttube to learn about paths if you are
-# not familiar with them.  They are important fundamental computer concepts.
-#
-# this is a full windows path, note the forward slashes '/' used in python
-# pathToFile = 'E:/Users/jerome/GitHub/evc-cit134a-python/birthday/birthday.json'
-#
-# mac (which is built on linux) and linux paths are like this: 'a/b/c/d/e/f.json'
-#
-
-# relative path
-# pathToFile = './birthday/birthday.json'
-
 # full path to file
-pathToFile = 'C:/Users/jerom/Documents/GitHub/class-python/birthday/birthday.json'
-
+pathToFile = r'C:\NGOCS\NGOC\CIS-024C Python\Lupes-Lookup-App\birthday.json'
 
 # try to open a file and throw a error if it is not found
 try:
@@ -24,7 +9,6 @@ try:
 except OSError:
     print('ERROR: Unable to open the file %s' % pathToFile)
     exit(-1)
-
 
 # read the whole json file into a variable
 birthday_list = json.load(json_file)
@@ -43,7 +27,6 @@ for elem in birthday_list:
     print('birthday = ' + birthday)
 
     birthday_dictionary[name] = birthday
-
 
 # to print a value in the dictionary by giving it a string with the name as the key
 print(f'Jocelyn Jones birthday is {birthday_dictionary['Jocelyn Jones']}')
